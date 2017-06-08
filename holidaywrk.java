a8d8d18aa19597a459c8b36402b4a0a888db5d28
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-08 12:21:22 +0530

    Create holidaywrk.java

---
+++ holidaywrk.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class holidaywrk
+{
+public static  void main(String args[])
+{
+if(args[0]=="sunday"|| args[0]=="saturday")
+System.out.print(false);
+else
+System.out.print(true);
+}
+}
+
