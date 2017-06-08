52b121bcdcdc7054cd978b3a9b2d3a756368d004
sruthiraghavan <sruthiraghav2096@gmail.com>
2017-06-08 12:14:28 +0530

    Create holiday.java

--- holiday.java
+++ holiday.java
@@ -1,1 +1,12 @@
-
+import java.*;
+class holiday
+{
+  public static void main(String args[])
+  {
+        
+           if(args[0]=="sunday"||args[0]=="saturday")
+              System.out.print("False");
+           else
+              System.out.print("True");
+              }
+              }
